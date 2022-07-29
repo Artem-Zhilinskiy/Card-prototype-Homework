91692deccbac55c74fd6dfcf58cd81e923f9d9f9
Artem-Zhilinskiy <Arterm313132@gmail.com>
2022-07-29 22:09:22 +0300

    29.07.2022
    Продолжаю делать заготовку со стрима

--- Card Prototype/Assets/Cards/Scripts/GameManager.cs
+++ Card Prototype/Assets/Cards/Scripts/GameManager.cs
@@ -7,13 +7,28 @@ namespace Cards
 {
     public class GameManager : MonoBehaviour
     {
+        private const float c_stepCardInDeck = 1f;
+
         private Material _baseMat;
         private List<CardPropertiesData> _allCards;
+
+        private Card[] _player1Deck;
+        private Card[] _player2Deck;
 
         [SerializeField]
         private CardPackConfiguration[] _packs;
         [SerializeField]
-        private Card _cradPrefab;
+        private Card _cardPrefab;
+
+        [SerializeField, Space]
+        private PlayerHand _playerHand1;
+        [SerializeField]
+        private Transform _player1DeckRoot;
+
+        [SerializeField, Space]
+        private PlayerHand _playerHand2;
+        [SerializeField]
+        private Transform _player2DeckRoot;
 
         [SerializeField, Space, Range(0f, 200f)]
         private int _cardDeckCount = 30;
@@ -25,6 +40,53 @@ namespace Cards
             _allCards = new List<CardPropertiesData>(arrayCards);
             _baseMat = new Material(Shader.Find("TextMeshPro/Sprite"));
             _baseMat.renderQueue = 2995;
+        }
+
+        private void Start()
+        {
+            _player1Deck = CreateDeck(_player1DeckRoot);
+            _player2Deck = CreateDeck(_player2DeckRoot);
+        }
+
+        private void Update()
+        {
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                var index = _player2Deck.Length - 1;
+                for (int i = index; i >= 0; i--)
+                {
+                    if (_player1Deck[i] != null)
+                    {
+                        index = i;
+                        break;
+                    }
+                }
+                _playerHand1.SetNewCard(_player1Deck[index]);
+                _player1Deck[index] = null;
+            }
+        }
+
+        private Card[] CreateDeck(Transform root)
+        {
+            //Здесь случайный набор, надо переделать в соответствии с ТЗ
+            var deck = new Card[_cardDeckCount];
+            var vector = Vector3.zero;
+            for (int i = 0; i<_cardDeckCount;i++)
+            {
+                deck[i] = Instantiate(_cardPrefab, root);
+                deck[i].transform.localPosition = vector;
+                if (deck[i].IsFrontSide) deck[i].SwitchEnable();
+                vector += new Vector3(0f,c_stepCardInDeck,0f);
+
+                var random = _allCards[Random.Range(0, _allCards.Count)];
+
+                var newMat = new Material(_baseMat)
+                {
+                    mainTexture = random.Texture
+                };
+                deck[i].Configuration(random, newMat, CardUtility.GetDescriptionById(random.Id));
+            }
+            return deck;
         }
     }
 }
---
+++ Card Prototype/Assets/Cards/Scripts/PlayerHand.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Cards
+{
+    public class PlayerHand : MonoBehaviour
+    {
+        private Card[] _cardsInHand;
+
+        [SerializeField]
+        private Transform[] _positions;
+
+        private void Start()
+        {
+            _cardsInHand = new Card[_positions.Length];
+        }
+
+        public bool SetNewCard(Card newCard)
+        {
+            var result = GetLastPosition();
+
+            if (result == -1)
+            {
+                Destroy(newCard.gameObject);
+                return false;
+            }
+            _cardsInHand[result] = newCard;
+            StartCoroutine(MoveInHand(newCard, _positions[result]));
+            return true;
+        }
+
+        private int GetLastPosition()
+        {
+            for (int i = 0; i < _cardsInHand.Length; i++)
+            {
+                if (_cardsInHand[i] == null) return i;
+            }
+            return -1;
+        }
+
+        private IEnumerator MoveInHand(Card card, Transform parent)
+        {
+            var time = 0f;
+            var startPos = card.transform.position;
+            var endPos = parent.position;
+            while (time <1f)
+            {
+                card.transform.position = Vector3.Lerp(startPos, endPos, time);
+                time += Time.deltaTime;
+                yield return null;
+            }
+        }
+    }
+}
